c3db9c4d0557bd592e752643ef906fdd186aba6d
Martin Macchia <mmacchia1178@gmail.com>
2022-04-13 21:31:36 -0300

    agregue archivo navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
+import Bootstrap from './components/NavBar'
 
 function App() {
   return (
+
+  <div>
   <h1>prueba 1</h1>
+  <NavBar/>
+  </div>
   );
+
+  
 }
 
 export default App;
